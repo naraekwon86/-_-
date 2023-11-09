ce54729a3b9316e2e693703b0e9a715d0890e745
naraekwon86 <wde0530@gmail.com>
2023-11-09 09:36:46 +0900

    ~6강

---
+++ src/main/java/com/ll/Quotation.java
@@ -0,0 +1,11 @@
+package com.ll;
+public class Quotation{
+    int id ;
+    String content;
+    String authorName;
+    public Quotation(){
+        this.id;
+        this.content;
+        this.authorName;
+    }
+}
